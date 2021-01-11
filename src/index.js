2c2940611f8a747f683f5b3e782740672795a931
yoshi <brown.toy.poodle@gmail.com>
2021-01-11 22:55:56 +0900

    add style.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 var smoothScroll = require('./smooth_scroll');
 
 $(function() {
-
+    $('.main.menu').visibility({
+        type: 'fixed'
+    });
+    // $('.overlay').visibility({
+    //   type: 'fixed',
+    //   offset: 80
+    // });
+         
+    // lazy load images
+    $('.image').visibility({
+        type: 'image',
+        transition: 'vertical flip in',
+        duration: 500
+    });
+    
+    // show dropdown on hover
+    $('.main.menu  .ui.dropdown').dropdown({
+        on: 'hover'
+    });
+    
     // メニュースクロール
     smoothScroll();
 });
